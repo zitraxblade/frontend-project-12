0ba242d33fca9ce6be5bdc2b9c7982f5f55e12d9
zitraxblade <egor50311@gmail.com>
2026-02-17 00:34:48 +0300

    11 etap

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,21 @@
 import 'react-toastify/dist/ReactToastify.css';
 import './i18n.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
 import App from './App.jsx';
 import './index.css';
 import { AuthProvider } from './auth/AuthProvider.jsx';
 import store from './store.js';
+
+import rollbar from './rollbar.js'; // ← ДОБАВИТЬ
+
+// чтобы можно было тестировать из консоли
+window.rollbar = rollbar;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
---
+++ frontend/src/rollbar.js
@@ -0,0 +1,10 @@
+import Rollbar from 'rollbar';
+
+const rollbar = new Rollbar({
+  accessToken: import.meta.env.VITE_ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  environment: 'development',
+});
+
+export default rollbar;
